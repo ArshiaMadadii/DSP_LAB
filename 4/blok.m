e845be9d00001ff6c5ba167693bb94a938d12afe
Arshia <ars.madadi@gmail.com>
2024-10-23 11:12:35 +0330

    2 edit

--- 4/blok.m
+++ 4/blok.m
@@ -1,30 +1,27 @@
 clc
 clear
 close all
-N = 120;
-L = 45;
-p = 15;
+
+N = 1000;
+L = 10;
+p = 5;
 
 x = ones(N,1);
 h = ones(p,1);
-
-x1 = zeros(N+p-1,1);
-x1(p:end) = x;
-
+y=conv(x,h);
 Ns = floor(N/L);
-Yc = zeros(L-p+1,1);
-
 yf=[];
 for j = 1:Ns
  for i = 1:L
-     Xs(i) = x1(i+(L-p+1)*(j-1)); 
- end
- xf = fft(Xs,L);
- hf = fft(h,L).';
- Yf = xf .* hf;
- ys = ifft(Yf,L);
- Yc(1:end) = ys(p:end);
- yf=[yf Yc];
-end
+     Xs(i) = x(i+L*(j-1)); 
 
+ end
+ ys=conv(Xs,h);
+ for i =1:L+p-1;
+    X2(j,i+(j-1)*L) = ys(i);
+ end
+end
+yf = sum(X2);
 stem(yf(1:35))
+hold on
+stem(y(1:35),"-*r")
